c16db195e34d656fa8b7d58a0de7d6c85dd6ce6f
Fanhsuan <38883631+Fanhsuan@users.noreply.github.com>
2018-08-02 20:21:27 +1000

    Add files via upload

---
+++ VINCE/Scripts/KeyAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyAnimation : MonoBehaviour {
+    private bool investigated;
+	// Use this for initialization
+	void Start () {
+        investigated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //When player press K to investigate 
+        //"Input.GetKey(KeyCode.L)" is just a subtitution of activating purple lens
+        if (Input.GetKey(KeyCode.K) && !investigated && Input.GetKey(KeyCode.L))
+            investigated = true;
+        
+        
+        //Move the key upward until it reach 3
+        if (transform.position.y < 3&&investigated)
+            transform.Translate(0, Time.deltaTime * 0.2f, 0, Space.World);
+        //Rotate the key when its moving upward
+        if (transform.position.y > 2.6)
+            transform.Rotate(Time.deltaTime *30 , 5*0.2f, 10*0.2f, Space.World);
+	}
+}
---
+++ VINCE/Scripts/SwitchTableTexture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTableTexture : MonoBehaviour {
+    public Texture originalTexture;
+    public Texture newTexture;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //When player press L, The texture changed.
+        //"Input.GetKey(KeyCode.L)" is just a subtitution of activating purple lens
+        if (Input.GetKey(KeyCode.L))
+            gameObject.GetComponent<Renderer>().material.mainTexture = newTexture;
+        //Otherwise stay normal texture
+        else
+            gameObject.GetComponent<Renderer>().material.mainTexture = originalTexture;
+    }
+}
